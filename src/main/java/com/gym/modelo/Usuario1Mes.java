217982d4438bc372e7135a059841607b7c3ac2b3
camilocardona-ing <camicarsu07@gmail.com>
2025-11-18 20:44:53 -0500

    Se creo el Main

--- src/main/java/com/gym/modelo/Usuario1Mes.java
+++ src/main/java/com/gym/modelo/Usuario1Mes.java
@@ -2,9 +2,8 @@ package com.gym.modelo;
 
 import java.time.LocalDate;
 
-/** Clase hija: plan de 1 mes */
 public class Usuario1Mes extends Usuario implements Verificable {
     public Usuario1Mes(String nombre, LocalDate fechaPago) { super(nombre, fechaPago, 1); }
-    @Override public double calcularPrecio() { return 80000.0; } // ejemplo
+    @Override public double calcularPrecio() { return 80000.0; } 
     @Override public boolean verificarDatos() { return nombre != null && !nombre.trim().isEmpty() && fechaPago != null; }
 }
