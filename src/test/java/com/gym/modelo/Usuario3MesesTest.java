3c54efc53994babc9ea68ec0d3658b36d2433777
demia <demia@Demian_pc>
2025-11-20 06:42:29 -0500

    Se creo el test de usuario3Meses

---
+++ src/test/java/com/gym/modelo/Usuario3MesesTest.java
@@ -0,0 +1,20 @@
+package com.gym.modelo;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+ 
+public class Usuario3MesesTest {   @Test
+    void usuario6Meses_debeCalcularPrecioYVencimientoCorrecto() {
+        LocalDate fechaPago = LocalDate.of(2025, 3, 15);
+        Usuario u = new Usuario6Meses("Felipe", fechaPago);
+
+        assertEquals(6, u.getMesesDuracion());
+        assertEquals(380000.0, u.calcularPrecio(), 0.0001);
+
+        LocalDate esperado = fechaPago.plusMonths(6);
+        assertEquals(esperado, u.getFechaVencimiento());
+    }
+}
+    
