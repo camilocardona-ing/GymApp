1e953904e73e0208f8929da5954d6c2689471726
demia <demia@Demian_pc>
2025-11-20 06:42:17 -0500

    Se creo el test de usuario1Mes

--- src/test/java/com/gym/modelo/Usuario1MesTest.java
+++ src/test/java/com/gym/modelo/Usuario1MesTest.java
@@ -1,13 +1,11 @@
 package com.gym.modelo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class UsuarioPlanesTest {
 
-    @Test
+public class Usuario1MesTest {  @Test
     void usuario1Mes_debeCalcularPrecioYVencimientoCorrecto() {
         LocalDate fechaPago = LocalDate.of(2025, 1, 10);
         Usuario u = new Usuario1Mes("Camilo", fechaPago);
@@ -18,11 +16,11 @@ public class UsuarioPlanesTest {
         LocalDate esperado = fechaPago.plusMonths(1);
         assertEquals(esperado, u.getFechaVencimiento());
 
-        // Antes o el mismo día del vencimiento → Activo
+        
         assertEquals("Activo", u.getEstado(esperado));
         assertEquals("Activo", u.getEstado(esperado.minusDays(1)));
 
-        // Un día después → Vencido
+        
         assertEquals("Vencido", u.getEstado(esperado.plusDays(1)));
     }
-
+}    
